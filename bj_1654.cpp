79b8a4d88f5bf854af37dcd3c2a527741f25f538
John Park <manpkh95@gmail.com>
2022-04-08 00:11:29 +0900

    4/8 1

---
+++ bj_1654.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+long long n, k;
+long long wire[10001];
+
+int main() {
+    cin >> n >> k;
+
+    long long input;
+    long long maxlen = 0;
+    for(int i = 0; i < n; i++) {
+        cin >> wire[i];
+        if (maxlen < wire[i]) maxlen = wire[i];
+    }
+
+
+    long long front = 0, back = maxlen + 1;
+
+    long long max = 0;
+
+    while (front < back) {
+        long long mid = (front+back)/2;
+        long long pieces = 0;
+
+        for (int i = 0; i < n; i++) {
+            pieces += wire[i] / mid;
+
+            if (pieces >= k) break;
+        }
+
+        if (pieces >= k) {
+            if (mid > max) max = mid;
+            front = mid + 1;
+        } else {
+            back = mid;
+        }
+
+    }
+
+    cout << max << endl;
+
+    return 0;
+}
